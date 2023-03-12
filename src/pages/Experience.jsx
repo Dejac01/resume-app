456b4b41a274b7e8da7581f98257b47f814c79e9
deja.craft <dejacraft28@gmail.com>
2023-03-12 17:14:25 -0400

    React-Resume

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 
-function Experience () {
-
-  return (
-    <div className='Experience'>
-       <h1>Experience</h1> 
-<ul>
-<ol></ol>
-<ol></ol>
-<ol></ol>
-</ul>
-    </div>
-  )
-}
+function Experience() {
+    return (
+      <div>
+  <h1>
+   Experience   
+  </h1>
+  <div className='Experience'>
+      <ul>
+      <div className='Experience1'> <ol>I.T Consultant: Inisht Enterprises</ol> 
+      <ol>2022-Present</ol></div>
+  
+       <div className='Experience2'> <ol>Banking/TechSUpport Analyst: Pnc Financial Services</ol> 
+        <ol> 2020-2022 </ol> </div>
+  
+       <div className='Experience3'> <ol>Communications Specialist: Loews Atlanta Hotel </ol> 
+     <ol> 2019-2020</ol> </div>
+      </ul>
+  </div>
+  </div>
+  
+    )
+  }
 
 export default Experience 
 
