d2c45b4dcd333ec1764a607daabd666f5ae1f153
deja.craft <dejacraft28@gmail.com>
2023-03-12 17:39:55 -0400

    React-Resume

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
     <div className="Home">
         <h1>Welcome to my Digital Resume!</h1> 
 
-        <img src="https://media0.giphy.com/media/jsSqMaSMji7PNyPXki/200w.gif?cid=82a1493b6xohrrg404s2r6u51mibaeuhns794jii8c2afijf&rid=200w.gif&ct=g" alt="React Image" />
+        <img className='imag1' src="https://media.tenor.com/RpKXrOa3eOAAAAAd/welcome.gif" alt="React Image" />
    
 
 
