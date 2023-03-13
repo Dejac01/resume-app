281b3997883078e858e388d4bc7fbe63f9348efb
deja.craft <dejacraft28@gmail.com>
2023-03-13 09:56:46 -0400

    React-Resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,20 +12,22 @@ I am an accomplished technology professional, writer and consultant with a prove
 </p>
 
 <div className='Links1'>
+
 <ul className='Links'>
-  <h1>Links</h1>
-  <ol><a href="https://www.linkedin.com/in/deja-craft/" rel="linkedin">
+
+  <ol><a className='Link3' href="https://www.linkedin.com/in/deja-craft/" rel="linkedin">
     Linked In
   </a></ol>
-  <ol><a href="https://www.clippings.me/dejac" rel="writing">
+  <ol><a className='Link3' href="https://www.clippings.me/dejac" rel="writing">
     My Writing
   </a></ol>
-  <ol><a href="https://github.com/Dejac01" rel="writing">
+  <ol><a className='Link3' href="https://github.com/Dejac01" rel="writing">
     My GitHub
   </a></ol>
 </ul>
     </div>
     </div>
+    
     </div>
   )
 }
