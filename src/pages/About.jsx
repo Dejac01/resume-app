ee26c1b3b879409d91b5b8604ddf93c3994ca24d
deja.craft <dejacraft28@gmail.com>
2023-03-13 09:41:54 -0400

    React-Resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,7 @@ About Me
 I am an accomplished technology professional, writer and consultant with a proven track record of leading teams through project management and supervisory roles. My skill set spans across diverse domains including banking compliance, data analysis, cybersecurity, hospitality, coding and programming languages, customer service, and team leadership. I possess an Associates degree in Business Administration and am currently pursuing a Bachelor of Science in Computer Science at Kennesaw State University. Additionally, I am actively engaged in a Google-sponsored software engineering program at PerScholas Technical School, which entails advanced training in JavaScript, computer science principles, and front and back end development. I am continuously learning and growing my skillset In hopes of merging my love for the sports and entertainment industry with software engineering and technology. 
 </p>
 
-<div>
+<div className='Links1'>
 <ul className='Links'>
   <h1>Links</h1>
   <ol><a href="https://www.linkedin.com/in/deja-craft/" rel="linkedin">
