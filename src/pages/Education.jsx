52f3ee9c01eabfe73ca38d4cc7be9e5b0c88ec19
deja.craft <dejacraft28@gmail.com>
2023-04-20 10:53:43 -0400

    React-Resume

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Education() {
   return (
     <div>
-<h1>
- Education   
-</h1>
-<div className='Education'>
-    <ul>
-    <div className='Education1'> <ol>PerScholas Tecnical School: Software Engineering
-     </ol> </div>
+      <h1>Education</h1>
+      <div className="Education">
+        <ul>
+          <div className="Education1">
+            {" "}
+            <ol>PerScholas Tecnical School: Software Engineering</ol>{" "}
+          </div>
 
-     <div className='Education2'> <ol>Associates Of Science: Business Administration</ol> 
-      <ol> Georgia Perimeter College </ol> </div>
+          <div className="Education2">
+            {" "}
+            <ol>Associates Of Science: Business Administration</ol>
+            <ol> Georgia Perimeter College </ol>{" "}
+          </div>
 
-     <div className='Education3'> <ol>Bachelors Of Science: Computer Science </ol> 
-   <ol> Louisiana State University </ol> </div>
-    </ul>
-</div>
-</div>
-
-  )
+          <div className="Education3">
+            {" "}
+            <ol>Bachelors Of Science: Computer Science </ol>
+            <ol> Kennesaw State University </ol>{" "}
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Education
+export default Education;
