8aa2507dd3f5f8ff941985dde1935b82f7d64b6e
deja.craft <dejacraft28@gmail.com>
2023-06-04 20:35:55 -0400

    changes

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -9,18 +9,19 @@ function Education() {
           <div className="Education1">
             {" "}
             <ol>PerScholas Tecnical School: Software Engineering</ol>{" "}
+            <ol> 2023-2023 </ol>{" "}
           </div>
 
           <div className="Education2">
             {" "}
             <ol>Associates Of Science: Business Administration</ol>
-            <ol> Georgia Perimeter College </ol>{" "}
+            <ol> Georgia Perimeter College </ol> <ol> 2015-2018 </ol>{" "}
           </div>
 
           <div className="Education3">
             {" "}
-            <ol>Bachelors Of Science: Computer Science </ol>
-            <ol> Kennesaw State University </ol>{" "}
+            <ol>Bachelors Of Science: Computer Science & Mathematics </ol>
+            <ol> Louisiana State University </ol> <ol> 2023-Current </ol>{" "}
           </div>
         </ul>
       </div>
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
+import React from "react";
 
 function Experience() {
-    return (
-      <div>
-  <h1>
-   Experience   
-  </h1>
-  <div className='Experience'>
-      <ul>
-      <div className='Experience1'> <ol>I.T Consultant: Insight Enterprises</ol> 
-      <ol>2022-Present</ol></div>
-  
-       <div className='Experience2'> <ol>Banking/Tech Support Analyst: Pnc Financial Services</ol> 
-        <ol> 2020-2022 </ol> </div>
-  
-       <div className='Experience3'> <ol>Communications Specialist: Loews Atlanta Hotel </ol> 
-     <ol> 2019-2020</ol> </div>
-      </ul>
-  </div>
-  </div>
-  
-    )
-  }
+  return (
+    <div>
+      <h1>Experience</h1>
+      <div className="Experience">
+        <ul>
+          <div className="Experience3">
+            {" "}
+            <ol> Software Developer Paid Internship: Health Equity </ol>
+            <ol> 2023- Present</ol>{" "}
+          </div>
 
-export default Experience 
+          <div className="Experience1">
+            {" "}
+            <ol>I.T Consultant: Insight Enterprises</ol>
+            <ol>2022-2023</ol>
+          </div>
 
+          <div className="Experience2">
+            {" "}
+            <ol>Banking/Tech Support Analyst: Pnc Financial Services</ol>
+            <ol> 2020-2022 </ol>{" "}
+          </div>
+
+          <div className="Experience3">
+            {" "}
+            <ol>I.T Analyst: Agent Please </ol>
+            <ol> 2018-2020</ol>{" "}
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Experience;
