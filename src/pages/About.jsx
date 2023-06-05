9f8477f7a3ea5331bf3e177ac5f97153634ce47f
deja.craft <dejacraft28@gmail.com>
2023-06-04 20:38:18 -0400

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,35 +1,59 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
     <div>
-<h1>
-About Me
-</h1>
-<div className='About'>
-<p>
-I am an accomplished technology professional, writer and consultant with a proven track record of leading teams through project management and supervisory roles. My skill set spans across diverse domains including banking compliance, data analysis, cybersecurity, hospitality, coding and programming languages, customer service, and team leadership. I possess an Associates degree in Business Administration and am currently pursuing a Bachelor of Science in Computer Science at Kennesaw State University. Additionally, I am actively engaged in a Google-sponsored software engineering program at PerScholas Technical School, which entails advanced training in JavaScript, computer science principles, and front and back end development. I am continuously learning and growing my skillset In hopes of merging my love for the sports and entertainment industry with software engineering and technology. 
-</p>
+      <h1>About Me</h1>
+      <div className="About">
+        <p>
+          I am an accomplished Digital Marketer, Web Designer, Writer, and
+          Software Developer with a proven track record of leading teams through
+          project management, digital marketing and supervisory roles. My skill
+          set spans across diverse domains including banking, compliance, data
+          analysis, marketing, hospitality, coding and programming languages,
+          customer service, and team leadership. I possess an Associates degree
+          in Business Administration and am currently pursuing a Bachelor of
+          Science in Computer Science and Mathematics at Louisiana State
+          University. Additionally, I have completed a Google-sponsored software
+          engineering program at PerScholas Technical School, which entailed
+          advanced training in JavaScript, computer science principles, and
+          front and back end development.
+        </p>
 
-<div className='Links1'>
-
-<ul className='Links'>
-
-  <ol><a className='Link3' href="https://www.linkedin.com/in/deja-craft/" rel="linkedin">
-    Linked In
-  </a></ol>
-  <ol><a className='Link3' href="https://www.clippings.me/dejac" rel="writing">
-    My Writing
-  </a></ol>
-  <ol><a className='Link3' href="https://github.com/Dejac01" rel="writing">
-    My GitHub
-  </a></ol>
-</ul>
+        <div className="Links1">
+          <ul className="Links">
+            <ol>
+              <a
+                className="Link3"
+                href="https://www.linkedin.com/in/deja-craft/"
+                rel="linkedin"
+              >
+                Linked In
+              </a>
+            </ol>
+            <ol>
+              <a
+                className="Link3"
+                href="https://www.clippings.me/dejac"
+                rel="writing"
+              >
+                My Writing
+              </a>
+            </ol>
+            <ol>
+              <a
+                className="Link3"
+                href="https://github.com/Dejac01"
+                rel="writing"
+              >
+                My GitHub
+              </a>
+            </ol>
+          </ul>
+        </div>
+      </div>
     </div>
-    </div>
-    
-    </div>
-  )
+  );
 }
 
-export default About
+export default About;
